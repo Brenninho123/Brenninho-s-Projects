fdc9cabe92ca204b0efa2197d4ca61445244a264
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-09-16 15:15:01 -0300

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,16 @@
+import turtle
+import print
+
+def draw_square(t, size):
+    
+    for _ in range(4):
+        t.forward(size)
+        t.backward(size)
+        t.right(90)
+        t.left(90)
+        t.backward(120)
+        t.forward(300)
+        
+        print='Hello World'
+        print='i am Turtle'
+        
