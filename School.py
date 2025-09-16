be917bcec73c5f805107a9254e30fcbf802425ce
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-09-16 19:26:28 -0300

    Update School.py

--- School.py
+++ School.py
@@ -31,7 +31,7 @@ for _ in range(4):
 
   for _ in range(3):
     s.forward(size)
-    s.backward(size)
+    s.backward()
     s.left(80)
     s.right(90)
     s.backward(50)
@@ -39,51 +39,122 @@ for _ in range(4):
     s.down(10)
     s.up(40)
 
-if math()
+if math():
     
-    calcule(1 + 1 = 2):
-    calcule(1 + 2 = 3):
-    calcule(2 + 2 = 4):
-    calcule(3 + 2 = 5):
-    calcule(3 + 3 = 6):
-    calcule(8 + 2 = 10):
-    calcule(9 + 9 = 18):
-    calcule(30 + 30 = 60):
-    calcule(60 + 60 = 120):
-    calcule(100 + 100 = 200):
-    calcule(200 + 200 = 400):
-    calcule(300 + 300 = 600):
-    calcule(400 + 400 = 800):
-    calcule(500 + 500 = 1000):
-    calcule(1000 + 1000 = 2000):
+    calcule(1 + 1)  # Result will be 2
+    calcule(1 + 2 ) # Result will be 3
+    calcule(2 + 2) # Result will be 4
+    calcule(3 + 2 = 5)
+    calcule(3 + 3 = 6)
+    calcule(8 + 2 = 10)
+    calcule(9 + 9 = 18)
+    calcule(30 + 30 = 60)
+    calcule(60 + 60 = 120)
+    calcule(100 + 100 = 200)
+    calcule(200 + 200 = 400)
+    calcule(300 + 300 = 600)
+    calcule(400 + 400 = 800)
+    calcule(500 + 500 = 1000)
+    calcule(1000 + 1000 = 2000)
+    calcule(2000 + 2000 = 4000)
+    calcule(4000 + 4000 = 8000)
 
-if english()
-read('hello'):
-read('hi'):
-read('hi there'):
-read('how are you today'):
+if english():
+read('hello')
+read('hi')
+read('hi there')
+read('how are you today')
 read('i like you')
 read('love')
 
- if portuguese()
- read('verb'):
+ if portuguese():
+ read('verb')
 
-if history()
-addHistoryList('WW1 - 1918'):
-addHistoryList('WW2 - 1939'):
-addHistoryLisy('Cold War - 1960'):
+if history():
+addHistoryList('WW1 - 1918')
+addHistoryList('WW2 - 1939')
+addHistoryLisy('Cold War - 1960')
 
-if geography()
-addCountry('Brazil'):
-addCountry('USA'):
-addCountry('Portugal'):
+if geography():
+addCountry('Brazil')
+addCountry('USA')
+addCountry('Portugal')
 addCountry('France')
+addCountry('Spain')
 
-if software()
+if software():
 def system_auto(system, size):
   def system_auto(size):
     def system_title(School Python):
       def system_local(Brazil)
+        def system_year(2024)
+          def system_month(June)
+            def system_day(26)
+              def system_hour(14:00)
+                def system_minute(30)
+                  def system_second(00)
+                    def system_millisecond(000)
+                      def system_microsecond(000)
+                        def system_nanosecond(000)
+                          def system_picosecond(000)
+                            def system_femtosecond(000)
+                              def system_attosecond(000)
+                                def system_zeptosecond(000)
+                                  def system_yottasecond(000)
+                                    def system_plancktime(000)
+                                      def system_lightyear(000)
+                                        def system_astronomicalunit(000)
+                                          def system_parsec(000)
+                                            def system_galaxy(000)
+                                              def system_universe(000)
+                                                def system_multiverse(000)
+                                                  def system_infinite(000)
+                                                    def system_eternity(000)
+                                                      def system_forever(000)
+                                                        def system_never(000)
+                                                          def system_always(000)
+                                                            def system_sometimes(000)
+                                                              def system_rarely(000)
+                                                                def system_usually(000)
+                                                                  def system_frequently(000)
+                                                                    def system_ocasionally(000)
+                                                                      def system_habitually(000)
+                                                                        def system_customarily(000)
+                                                                          def system_commonly(000)
+                                                                            def system_generally(000)
+                                                                              def system_normally(000)
+                                                                                def system_typically(000)
+                                                                                  def system_regularly(000)
+                                                                                    def system_consistently(000)
+                                                                                      def system_dependably(000)
+                                                                                        def system_reliably(000)
+                                                                                          def system_stably(000)
+                                                                                            def system_securely(000)
+                                                                                              def system_safely(000)
+                                                                                                def system_properly(000)
+                                                                                                  def system_correctly(000)
+                                                                                                    def system_accurately(000)
+                                                                                                      def system_precisely(000)
+                                                                                                        def system_exactly(000)
+                                                                                                          def system_perfectly(000)
+                                                                                                            def system_flawlessly(000)
+                                                                                                              def system_seamlessly(000)
+                                                                                                                def system_effortlessly(000)
+                                                                                                                  def system_smoothly(000)
+                                                                                                                    def system_easily(000)
+                                                                                                                      def system_quickly(000)
+                                                                                                                        def system_fastly(000)
+                                                                                                                          def system_speedily(000)
+                                                                                                                            def system_briskly(000)
+                                                                                                                              def system_promptly(000)
+                                                                                                                                def system_immediately(000)
+                                                                                                                                  def system_instantly(000)
+                                                                                                                                    def system_directly(000)
+                                                                                                                                      def system_straightaway(000)
+                                                                                                                                        def system_rightaway(000)
+                                                                                                                                          def system_withoutdelay(000)
+                                                                                                                                            def system_atonce(000)
+                                                                                                                                              def system_now(000)
 
 print='circle'
 print='square'
